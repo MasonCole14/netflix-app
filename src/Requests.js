606059a799017e92e5ee070f9d36f80e01bf24dd
Mason Cole <cole.mason2@gmail.com>
2023-07-23 18:16:53 -0500

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "947d301cf0a9feb40dca6a4ab5d78bcc";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests
