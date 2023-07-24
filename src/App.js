81de3adc8d03989e0b3273020461716e2ce806d7
Mason Cole <cole.mason2@gmail.com>
2023-07-24 15:08:36 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
