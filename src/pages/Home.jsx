830ce0e409f08974a83c05d93a7c8494363bb6ee
Mason Cole <cole.mason2@gmail.com>
2023-07-25 15:08:30 -0500

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <div>
       <Main />
-      <Row title="UpComing" fetchURL={requests.requestUpcoming} />
-      <Row title="Popular" fetchURL={requests.requestPopular} />
-      <Row title="Trending" fetchURL={requests.requestTrending} />
-      <Row title="Top Rated" fetchURL={requests.requestTopRated} />
-      <Row title="Latest" fetchURL={requests.requestLatest} />
+      <Row rowID='1' title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID='2' title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID='3' title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID='4' title="Latest" fetchURL={requests.requestLatest} />
+      <Row rowID='5' title="Coming Soon" fetchURL={requests.requestUpcoming} />
     </div>
   );
 };
